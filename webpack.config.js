f584cab20c3a3334a951c32966d0d3cad9dc614e
Marcos Henrique <marcoshenriqueh393@gmail.com>
2022-11-26 09:52:50 -0300

    criando o projeto raiz 🚀

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path'); // usa o path para garantir que sempre vai achar o arquivo
+
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'), // normalmente o que tem o REACTDOM.render()
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle[hash].js',
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+  }
+};
